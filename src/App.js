37f5f14c35d4a5f9c320d7f3cb0430d10f565105
unknown <97510320008@ispace.edu.vn>
2024-06-13 13:41:16 +0700

    bsdde1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Admin from './components/Admin/Admin';
 function App() {
   return (
     <div>
-      <Router basename='thagispace.github.io'>
+      <Router basename='/thagReact'>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Main />} />
