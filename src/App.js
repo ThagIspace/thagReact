fd693b36f3acac4743e163e70e512c545f7be932
unknown <97510320008@ispace.edu.vn>
2024-06-13 11:43:00 +0700

    fix again 404

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Admin from './components/Admin/Admin';
 function App() {
   return (
     <div>
-      <Router basename='/'>
+      <Router basename='/thagReact'>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Main />} />
